5e637df06cf1cee72f017159a085d809b4aaf49a
Unnikrishnan Menon <menon.uk1998@gmail.com>
2022-07-09 19:35:41 +0530

    code re-structured

---
+++ car.py
@@ -0,0 +1,33 @@
+from math import sin, radians, degrees
+from pygame.math import Vector2
+
+class Car:
+    def __init__(self, x, y, ppu=32,angle=0.0, length=4, max_steering=30, max_acceleration=5.0):
+        self.position = Vector2(x, y)
+        self.velocity = Vector2(0.0, 0.0)
+        self.angle = angle
+        self.length = length
+        self.max_acceleration = max_acceleration
+        self.max_steering = max_steering
+        self.max_velocity = 20
+        self.brake_deceleration = 10
+        self.free_deceleration = 2
+
+        self.acceleration = 0.0
+        self.steering = 0.0
+        self.raycast_startpoint = self.position * ppu
+        self.raycast_endpoint = self.position * ppu + Vector2(300, 0)
+        self.raycast_current_endpoint = None
+
+    def update(self, dt):
+        self.velocity += (self.acceleration * dt, 0)
+        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
+
+        if self.steering:
+            turning_radius = self.length / sin(radians(self.steering))
+            angular_velocity = self.velocity.x / turning_radius
+        else:
+            angular_velocity = 0
+
+        self.position += self.velocity.rotate(-self.angle) * dt
+        self.angle += degrees(angular_velocity) * dt
--- game.py
+++ game.py
@@ -2,44 +2,12 @@ import os
 import pygame
 from math import sin, radians, degrees, copysign
 from pygame.math import Vector2
-
-
-class Car:
-    def __init__(self, x, y, ppu=32,angle=0.0, length=4, max_steering=30, max_acceleration=5.0):
-        self.position = Vector2(x, y)
-        self.velocity = Vector2(0.0, 0.0)
-        self.angle = angle
-        self.length = length
-        self.max_acceleration = max_acceleration
-        self.max_steering = max_steering
-        self.max_velocity = 20
-        self.brake_deceleration = 10
-        self.free_deceleration = 2
-
-        self.acceleration = 0.0
-        self.steering = 0.0
-        self.raycast_startpoint = self.position * ppu
-        self.raycast_endpoint = self.position * ppu + Vector2(300, 0)
-        self.raycast_current_endpoint = None
-
-    def update(self, dt):
-        self.velocity += (self.acceleration * dt, 0)
-        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
-
-        if self.steering:
-            turning_radius = self.length / sin(radians(self.steering))
-            angular_velocity = self.velocity.x / turning_radius
-        else:
-            angular_velocity = 0
-
-        self.position += self.velocity.rotate(-self.angle) * dt
-        self.angle += degrees(angular_velocity) * dt
-
+from car import Car
 
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption("Car tutorial")
+        pygame.display.set_caption("Racer_AI")
         width = 1280
         height = 720
         self.screen = pygame.display.set_mode((width, height))
@@ -49,7 +17,7 @@ class Game:
 
     def run(self):
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        image_path = os.path.join(current_dir, "car.png")
+        image_path = os.path.join(current_dir, "assets/car.png")
         car_image = pygame.image.load(image_path)
         ppu = 32
         car = Car(0, 0, ppu=ppu)
