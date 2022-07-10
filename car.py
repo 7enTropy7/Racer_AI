f657372a0fe57921850fed777751bc2e12187299
Unnikrishnan Menon <menon.uk1998@gmail.com>
2022-07-11 00:45:26 +0530

    car-wall collisions working

--- car.py
+++ car.py
@@ -5,14 +5,7 @@ import math
 from math import sin, radians, degrees, copysign, cos
 from pygame.math import Vector2
 from raycast import Raycast
-
-def rotate(origin, point, angle):
-    ox, oy = origin
-    px, py = point
-
-    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
-    return qx, qy
+from utils import rotate_point
 
 class Car:
     def __init__(self, x, y, ppu=32, angle=0.0, length=4, max_steering=30, max_acceleration=5.0):
@@ -79,6 +72,20 @@ class Car:
 
         self.update(walls, dt)
 
+        collision_flag = False
+        for wall in walls:
+            front_collision = self.check_collision(wall, self.f_l, self.f_r) #front
+            back_collision = self.check_collision(wall, self.b_l, self.b_r) #back
+            left_collision = self.check_collision(wall, self.f_l, self.b_l) #left
+            right_collision = self.check_collision(wall, self.f_r, self.b_r) #right
+
+            if(front_collision or back_collision or left_collision or right_collision):
+                collision_flag = True
+                break
+        
+        if collision_flag:
+            print('Collision!')
+            sys.exit(1)
 
     def update(self, walls, dt):
         self.velocity += (self.acceleration * dt, 0)
@@ -102,10 +109,47 @@ class Car:
         self.b_l = (self.position.x * self.ppu - (128/2), self.position.y * self.ppu - (64/2))
         self.b_r = (self.position.x * self.ppu - (128/2), self.position.y * self.ppu + (64/2))
     
-        self.f_l = rotate(self.position * self.ppu, self.f_l, radians(-self.angle))
-        self.f_r = rotate(self.position * self.ppu, self.f_r, radians(-self.angle))
-        self.b_l = rotate(self.position * self.ppu, self.b_l, radians(-self.angle))
-        self.b_r = rotate(self.position * self.ppu, self.b_r, radians(-self.angle))
+        self.f_l = rotate_point(self.position * self.ppu, self.f_l, radians(-self.angle))
+        self.f_r = rotate_point(self.position * self.ppu, self.f_r, radians(-self.angle))
+        self.b_l = rotate_point(self.position * self.ppu, self.b_l, radians(-self.angle))
+        self.b_r = rotate_point(self.position * self.ppu, self.b_r, radians(-self.angle))
+
+    def check_collision(self, wall, line_startpoint, line_endpoint):
+        x1 = line_startpoint[0]
+        y1 = line_startpoint[1]
+        x2 = line_endpoint[0]
+        y2 = line_endpoint[1]
+        
+        x3 = wall.start_pos[0]
+        y3 = wall.start_pos[1]
+        x4 = wall.end_pos[0]
+        y4 = wall.end_pos[1]
+
+        if(x1 == x2): 
+            return False
+
+        if(x3 == x4):
+            if((x3 > min(x1,x2) and x3 < max(x1,x2)) and (min(y3,y4) < min(y1,y2) and max(y3,y4) > max(y1,y2))):
+                return True
+            else:
+                return False
+
+        a1 = (y1-y2)/(x1-x2)  
+        a2 = (y3-y4)/(x3-x4) 
+        b1 = y1-a1*x1 
+        b2 = y3-a2*x3 
+
+        if (a1 == a2):
+            return False  # Parallel segments
+
+        xa = (b2 - b1) / (a1 - a2)
+
+        if ((xa < max(min(x1,x2), min(x3,x4))) or
+            (xa > min(max(x1,x2), max(x3,x4)))):
+            return False  # intersection is out of bound
+        else:
+            return True
+
 
     def state(self):
         return [self.ray_l.ray_length, self.ray_c.ray_length, self.ray_r.ray_length]
@@ -115,9 +159,9 @@ class Car:
         self.ray_l.draw(screen)
         self.ray_r.draw(screen)
 
-        rotated = pygame.transform.rotate(self.car_image, self.angle)
-        rect = rotated.get_rect()
-        screen.blit(rotated, self.position * self.ppu - (rect.width / 2, rect.height / 2))
+        rotated_car = pygame.transform.rotate(self.car_image, self.angle)
+        rect = rotated_car.get_rect()
+        screen.blit(rotated_car, self.position * self.ppu - (rect.width / 2, rect.height / 2))
 
         pygame.draw.circle(screen, (0,255,0), center=self.f_l, radius=5, width=2)
         pygame.draw.circle(screen, (0,255,0), center=self.f_r, radius=5, width=2)
--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
 from wall import Wall
 import random
+import math
+
+def rotate_point(origin, point, angle):
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
 
 def generate_border_walls(screen_dims):
     walls = []
@@ -16,6 +25,8 @@ def generate_border_walls(screen_dims):
         end_y = random.randint(0, screen_dims[1])
         walls.append(Wall((start_x, start_y), (end_x, end_y)))
 
+    # walls.append(Wall((700, 200), (700, 500)))
+
     return walls
 
 def generate_track_walls(screen_dims):
@@ -26,11 +37,4 @@ def generate_track_walls(screen_dims):
     walls.append(Wall((screen_dims[0], 0), (screen_dims[0], screen_dims[1])))
     walls.append(Wall((0, screen_dims[1]), (screen_dims[0], screen_dims[1])))
     
-    # for i in range(3):
-    #     start_x = random.randint(0, screen_dims[0])
-    #     start_y = random.randint(0, screen_dims[1])
-    #     end_x = random.randint(0, screen_dims[0])
-    #     end_y = random.randint(0, screen_dims[1])
-    #     walls.append(Wall((start_x, start_y), (end_x, end_y)))
-
     return walls
